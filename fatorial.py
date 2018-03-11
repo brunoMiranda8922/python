219b426c340c7a2acb7e67721ed43d401a86cf79
Bruno Miranda <bruno.silva@catho.com>
2018-03-11 04:00:56 -0300

    Fatorial utilizando While

--- fatorial.py
+++ fatorial.py
@@ -1,4 +1,12 @@
-from math import factorial
-fatorial = 5
-f = factorial(fatorial)
-print(f)
+# from math import factorial
+# fatorial = 5
+# f = factorial(fatorial)
+# print(f)
+
+fatorial = 1
+contador = 1 
+while contador <= 10:
+    
+    fatorial = fatorial * contador
+    contador = contador + 1
+    print(fatorial)
