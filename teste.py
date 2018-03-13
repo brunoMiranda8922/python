c7818505ae8ca986260478ec13c8d6cfbbfb586c
Bruno Miranda <bruno.silva@catho.com>
2018-03-13 19:26:46 -0300

    Utilizando Dicionário

---
+++ teste.py
@@ -0,0 +1,16 @@
+dic = {"Nome": "Bruno", "Idade": "21 anos"}
+print(dic, '\n') #Exibe todos os elementos do dicionário. 
+
+print(dic["Nome"], dic["Idade"], '\n') #Retornando o valor associado à uma chave, notação de indices[]
+
+dic["Nome"] = "Jose" #Atribuindo um novo valor na chave "Nome" 
+print(dic, '\n') 
+
+
+ 
+
+dic["Nome2"] = "Messi" #Adicionando um novo Item ao dic, [Chave] = "Valor"
+dic["Idade2"] = "22 anos"
+print(dic["Nome"],dic["Idade"],'   ', dic["Nome2"], dic["Idade2"])
+
+
